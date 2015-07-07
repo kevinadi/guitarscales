0ec48e9fa4ce29b268efafea2319e4471e70d731
Kevin Adistambha <kevinadi@gmail.com>
2015-07-07 17:23:39 +1000

    WIP: guitarscales.hs

--- guitarscales.hs
+++ guitarscales.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.List.Split
 import Data.Maybe
+import Control.Monad
 
 scales = [  ("Chromatic"        , "1,b2,2,b3,3,4,b5,5,b6,6,b7,7"),
             ("Major"            , "1,2,3,4,5,6,7"),
@@ -27,9 +28,9 @@ chords = [  ("Major"            , "1,3,5"),
             ("Augmented"        , "1,3,#5"),
             ("Diminished"       , "1,b3,b5") ]
 
-scaledict=[ ("scales", scales),
-            ("modes" , modes),
-            ("chords", chords) ]
+scaledict=[ ("Scale", scales),
+            ("Mode" , modes),
+            ("Chord", chords) ]
 
 notes_flat =  ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 notes_sharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
@@ -42,42 +43,54 @@ noteindex :: String -> Int
 noteindex n = fromMaybe 0 . elemIndex n $ case last n == '#' of
     True  -> notes_sharp
     False -> notes_flat
-
-scaleintvl :: String -> [Int]
-scaleintvl s = 
-    let s' = splitOn "," s
-        intvl t = catMaybes . map (flip elemIndex t) $ s'
-    in intvl $ case any (== '#') s of
-        True  -> interval_sharp
-        False -> interval_flat        
-
-scaleintvl' :: Int -> String -> [Int]
-scaleintvl' tr s = 
+    
+scaleintvl :: Int -> String -> [Int]
+scaleintvl tr s = 
     let s' = splitOn "," s
         intvl t = map (\n -> (n+tr) `mod` 12) . catMaybes . map (flip elemIndex t) $ s'
     in intvl $ case any (== '#') s of
         True  -> interval_sharp
         False -> interval_flat     
 
-scalenotes :: String -> String -> [String]
-scalenotes t s =
-    let s' = scaleintvl s
-        t' = noteindex t
-        s'' = map (\n -> (n+t') `mod` 12) s'
-        notelist = flip map s'' . (!!)  --notelist notes = map ((!!) notes) s''
-    in notelist $ case any (== '#') s of
-        True  -> notes_sharp
-        False -> notes_flat
-
 scalelist :: String -> [String]
 scalelist s = fromJust $ lookup s scaledict >>= \a -> return (map fst a)
 
+fretboard :: [Int] -> [[Maybe Int]]
+fretboard scale = [ [ if fret s f `elem` scale then Just (fret s f) else Nothing | f <- [0..14] ] | s <- strings ] 
+    where fret s f = (s+f) `mod` 12
 
-fretboard = [[ (s+f) `mod` 12 | f<-[0..15] ] | s<-strings ]
+shownote :: Maybe Int -> String -> String -> String
+shownote mnote pre post = 
+    let stringnote i = notes_flat !! i
+    in case mnote of
+        Just note ->
+            let n = stringnote note
+            in case length n of
+                2 -> (pre ++ n ++ (tail post))
+                _ -> (pre ++ n ++ post)
+        Nothing -> (pre ++ [head post] ++ post)
 
+printfretboard :: [[Maybe Int]] -> IO ()
+printfretboard (f:fs) = putStr (shownote (head f) "" " |") >> printstring (tail f) >> printfretboard fs
+printfretboard []     = putStrLn dots
+
+printstring :: [Maybe Int] -> IO ()
+printstring (s:ss) = putStr (shownote s "|--" "--") >> printstring ss
+printstring []     = putStrLn []   
+
+lookupscale :: [String] -> Maybe [Int]
+lookupscale cfg = lookup (head cfg) scaledict >>= lookup (last cfg) >>= return . scaleintvl (noteindex (cfg!!1))
+
+dots :: String
+dots = "                  o           o           o           o                o o"
 
 main :: IO ()
 main = do
-    print $ fromJust $ lookup "scales" scaledict >>= return . map fst
-    print $ fromJust $ lookup "scales" scaledict >>= lookup "Major" >>= return . scalenotes "C"
-    print $ fromJust $ lookup "scales" scaledict >>= lookup "Natural Minor" >>= return . scalenotes "A"
+    let cfg = ["Scale","G","Major"]
+    let cfg = ["Chord","G","Major"]
+    let key = noteindex (cfg!!1)
+    let scale = fromJust $ lookupscale cfg >>= return . map Just
+    let frets = fromJust $ lookupscale cfg >>= return . fretboard
+
+    putStrLn . init . concatMap (\i -> shownote i "  " "  -") $ scale
+    printfretboard frets
